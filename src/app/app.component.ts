3082f85c784d2d38d05078985f63a720f19324bc
WI1L0 <111997657+WI1L0@users.noreply.github.com>
2024-07-04 18:02:58 -0500

    echo
    jj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'farmacia';
+
+  order = {
+    medicamento: '',
+    tipo: '',
+    cantidad: null,
+    distribuidor: '',
+    sucursal: {
+      principal: false,
+      secundaria: false
+    },
+    sucursalAddress: ''
+  };
+
+  showSummary = false;
+
+  submitOrder(form: any) {
+    if (form.valid) {
+      if (!this.order.sucursal.principal && !this.order.sucursal.secundaria) {
+        alert('Debe seleccionar al menos una sucursal.');
+        return;
+      }
+      this.order.sucursalAddress = this.getSucursalAddress();
+      this.showSummary = true;
+    }
+  }
+
+  resetForm(form: any) {
+    form.resetForm();
+    this.order.sucursal = { principal: false, secundaria: false };
+  }
+
+  closeSummary() {
+    this.showSummary = false;
+  }
+
+  sendOrder() {
+    console.log('Pedido Enviado');
+    this.showSummary = false;
+    this.resetForm({ resetForm: () => {} });
+  }
+
+  getSucursalAddress(): string {
+    const addresses = [];
+    if (this.order.sucursal.principal) addresses.push('Calle 12 de Diciembre');
+    if (this.order.sucursal.secundaria) addresses.push('Calle Av. Quito');
+    return addresses.join(' y ');
+  }
 }
