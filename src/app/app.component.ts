e2560f0e9d1ca1538d8ce2e667b2b03933b02e85
WI1L0 <william.martinez.est@tecazuay.edu.ec>
2024-07-08 14:05:54 -0500

    completado
    validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,56 +6,102 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  nombreMedicamento: string = '';
+  tipoMedicamento: string = '';
+  cantidadProducto: number = 0;
+  distribuidor: string = '';
+  principal: boolean = false;
+  secundaria: boolean = false;
 
-  order = {
-    medicamento: '',
+  alerta = {
+    mostrar: false,
+    mensaje: '',
     tipo: '',
-    cantidad: null,
-    distribuidor: '',
-    sucursal: {
-      principal: false,
-      secundaria: false
-    },
-    sucursalAddress: ''
   };
 
-  showSummary = false;
+  mostrarModal: boolean = false;
+  resumenPedido: string = '';
 
-  submitOrder(form: any) {
-    if (form.valid) {
-      if (!this.order.sucursal.principal && !this.order.sucursal.secundaria) {
-        alert('Debe seleccionar al menos una sucursal.');
-        return;
-      }
-      this.order.sucursalAddress = this.getSucursalAddress();
-      this.showSummary = true;
+  permitirSoloLetras(event: KeyboardEvent) {
+    const regex = /^[a-zA-Z\s]*$/;
+    const key = String.fromCharCode(event.keyCode);
+    if (!regex.test(key)) {
+      event.preventDefault();
     }
   }
 
-  resetForm(form: any) {
-    form.resetForm();
-    this.order.sucursal = { principal: false, secundaria: false };
+  mostrarAlerta(mensaje: string, tipo: string) {
+    this.alerta.mostrar = true;
+    this.alerta.mensaje = mensaje;
+    this.alerta.tipo = tipo;
+    setTimeout(() => {
+      this.alerta.mostrar = false;
+    }, 3000);
   }
 
-  closeSummary() {
-    this.showSummary = false;
+  mostrarResumenPedido() {
+    if (
+      !this.nombreMedicamento ||
+      !this.tipoMedicamento ||
+      !this.cantidadProducto ||
+      !this.distribuidor ||
+      (!this.principal && !this.secundaria)
+    ) {
+      this.mostrarAlerta(
+        'Por favor complete todos los campos.',
+        'border-red-500 text-red-500'
+      );
+      return;
+    }
+
+    if (this.cantidadProducto <= 0) {
+      this.mostrarAlerta(
+        'La cantidad del producto debe ser mayor que cero.',
+        'border-red-500 text-red-500'
+      );
+      return;
+    }
+
+    let direccionFarmacia = '';
+    if (this.principal && this.secundaria) {
+      direccionFarmacia =
+        'Para la farmacia situada en Calle 12 de Diciembre y para la situada en Calle Av. Quito.';
+    } else if (this.principal) {
+      direccionFarmacia = 'Para la farmacia situada en Calle 12 de Diciembre.';
+    } else if (this.secundaria) {
+      direccionFarmacia = 'Para la farmacia situada en Calle Av. Quito.';
+    }
+    this.resumenPedido = `Pedido al Distribuidor ${this.distribuidor}. \n\n ${this.cantidadProducto} unidades del ${this.tipoMedicamento} ${this.nombreMedicamento}. \n\n ${direccionFarmacia}`;
+    this.mostrarModal = true;
   }
 
-  sendOrder() {
-    console.log('Pedido Enviado');
-    this.showSummary = false;
-    this.resetForm({ resetForm: () => {} });
+  confirmarPedido() {
+    this.mostrarModal = false;
+    console.log(this.resumenPedido);
+    this.mostrarAlerta('Pedido Enviado', 'border-green-500 text-green-500');
   }
 
-  getSucursalAddress(): string {
-    const addresses = [];
-    if (this.order.sucursal.principal) addresses.push('Calle 12 de Diciembre');
-    if (this.order.sucursal.secundaria) addresses.push('Calle Av. Quito');
-    return addresses.join(' y ');
+  cancelarPedido() {
+    this.mostrarModal = false;
+    this.mostrarAlerta('Pedido Cancelado', 'border-red-500 text-red-500');
+  }
+
+  borrarDatos() {
+    this.nombreMedicamento = '';
+    this.tipoMedicamento = '';
+    this.cantidadProducto = 0;
+    this.distribuidor = '';
+    this.principal = false;
+    this.secundaria = false;
+    this.alerta = {
+      mostrar: false,
+      mensaje: '',
+      tipo: '',
+    };
   }
 }
